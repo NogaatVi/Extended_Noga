7c1ad7b3f5fd6459ffd7eab02174880c2496c839
NogaatVi <noga@vi.co>
2024-09-03 19:22:49 +0300

    scripts

---
+++ Extended3/DungeonCrawler/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace DungeonCrawler;
+
+class Program
+{
+    public class Loot
+    {
+        
+
+    }
+
+    public class Room
+    {
+        List<Creature> monsterList = new List<Creature> { };
+        List<Loot> lootList = new List<Loot> { };
+
+    }
+
+    public class Dungeon
+    {
+        List<Room> roomList = new List<Room> { };
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Dungeon Crawler let's go");
+    }
+}
+
