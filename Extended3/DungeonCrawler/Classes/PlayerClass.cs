7943d102d36e44cbfbfd4f4aaa3a3bfd8f4757aa
NogaatVi <noga@vi.co>
2024-09-04 19:35:57 +0300

    work in work

---
+++ Extended3/DungeonCrawler/Classes/PlayerClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Extended3.DungeonCrawler.Classes
+{
+    public class Player
+    {
+        public int level;
+        public int power;
+        public int health;
+        public string name = "";
+
+        public Player(int level, int power, int health, string name) //for player
+        {
+            this.level = level;
+            this.power = power;
+            this.health = health;
+            this.name = name;
+        }
+        public string playerNamer()
+        {
+            Console.WriteLine("What is your name, brave dungeon delver?");
+            return name;
+        }
+
+        public int reduceHealth() //if power < monsterPo
+        {
+            return health;
+        }
+        public int regenerateHealth()
+        {
+            return health;
+        }
+    }
+}
+
