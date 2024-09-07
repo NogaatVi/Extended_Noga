8fddeaba5b96cdb7c46fabb2ed9cbb495c3b7bbb
Doronnoga <doronnoga@gmail.com>
2024-09-07 14:47:40 +0300

    Update Program.cs

--- Extended3/DungeonCrawler/Program.cs
+++ Extended3/DungeonCrawler/Program.cs
@@ -9,21 +9,26 @@ class Program
     {
         List<Monster> monsterList = new List<Monster> { };
         //List<Loot> lootList = new List<Loot> { };
+        public Room()
+        {
+
+        }
     }
 
     public class Dungeon
     {
-        List<Room> roomList = new List<Room> { };
-        public Dungeon(List<Room> roomList) 
+        List<Room> roomList = new List<Room>{};
+        Player player = new Player(0 , 10 , 25);
+
+        public Dungeon()//List<Room> roomList) 
         { 
-            this.roomList = roomList;
+            //this.roomList = roomList;
         }
         public void InitializeDungeon() 
         {
             Console.WriteLine("Welcome, dear adventurer, you are now at the gates of the Mystic Dungeon!");
             Console.WriteLine("Be wary, for inside great treasure and even greater danger awaits...");
-            Player MyPlayer = new Player(0, 10, 25);
-            MyPlayer.playerNamer;
+            player.playerNamer();
 
         }
     }
@@ -31,7 +36,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Dungeon Crawler let's go");
-        
+        Dungeon thisDungeon = new Dungeon();
+        thisDungeon.InitializeDungeon();
         Console.ReadLine();
     }
 }
