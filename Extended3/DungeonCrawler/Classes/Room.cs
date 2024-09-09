2f109bce1c430aa19a827e39a8577ef53bd29b21
NogaatVi <noga@vi.co>
2024-09-09 20:01:47 +0300

    preparing fro encounter
    started writin fight and flee

--- Extended3/DungeonCrawler/Classes/Room.cs
+++ Extended3/DungeonCrawler/Classes/Room.cs
@@ -11,14 +11,21 @@ namespace roomClass
         public string Name { get; set; }
         Loot loot = new Loot(0, "");
         Monster Monster = new Monster(0,0);
+        bool win = false;
         public static int roomsInitialized = 0;
         public Room(string name)
         {
             roomsInitialized++;
             Name = name;
         }
+        public void BracketPutter()//i want a bracket here
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("---------------------------------------------------");
+        }
         public void InitializeLoot() //get loot
         {
+            BracketPutter();
             Console.WriteLine("Upon entering, you spot a curious object:");
             loot.lootNamer();
             loot.lootPowerSetter();
@@ -26,22 +33,29 @@ namespace roomClass
         }
         public void InitializeMonster() //get monster
         {
+            BracketPutter();
             Monster.titleGiver();
             Monster.nameSetter();
             Monster.monsterPowerSetter();
             Monster.monsterAnnouncer();
         }
+        public bool Fight()
+        {
+            return win;
+        }
         public void Encounter(Player player)// finish encounter- compare monster and player power. bool win. 
         {
+            BracketPutter();
             Console.WriteLine($"You've entered {Name}");
             InitializeLoot();
             InitializeMonster();
             Console.WriteLine($"Will you flee, or will you fight?");
-            if ("fight".Equals(Console.ReadLine(), StringComparison.OrdinalIgnoreCase)) 
+            string action = Console.ReadLine();
+            if ("fight".Equals(action, StringComparison.OrdinalIgnoreCase)) 
             { 
                 Console.WriteLine("Fight!");
             }
-            else if ("flee".Equals(Console.ReadLine(), StringComparison.OrdinalIgnoreCase)) 
+            else if ("flee".Equals(action, StringComparison.OrdinalIgnoreCase)) 
             {
                 Console.WriteLine("You've fled...");
             }
